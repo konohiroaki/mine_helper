78745ad44692c61ce8473d4cfe76319e9333636c
Hiroaki Kono <hiroakikono1990@gmail.com>
2016-05-12 01:19:20 +0900

    added docstring and better for loops

--- mine_solver.py
+++ mine_solver.py
@@ -1,6 +1,7 @@
-"""
-Solves Minesweeper.
-Receives a board[][] and returns a list of cell & action indicating where we can open/flag.
+"""Solves Minesweeper.
+
+Receives: A board[][].
+Returns: A list of cell & action indicating where you can open/flag.
 """
 
 from mine_const import Const
@@ -34,50 +35,48 @@ def get_board_status(board):
 
 def __count_cell_with(board, status):
     count = 0
-    for y, row in enumerate(board):
-        for x, cell in enumerate(board[y]):
-            if board[y][x] is status:
-                count += 1
+    for y, x in ((y, x) for y in range(len(board)) for x in range(len(board[0]))):
+        if board[y][x] is status:
+            count += 1
     return count
 
 
 def __get_cell_with(board, status):
     cell_list = []
-    for y, row in enumerate(board):
-        for x, cell in enumerate(board[y]):
-            if board[y][x] is status:
-                cell_list.append([y, x])
+    for y, x in ((y, x) for y in range(len(board)) for x in range(len(board[0]))):
+        if board[y][x] is status:
+            cell_list.append([y, x])
     return cell_list
 
 
 def __solve_by_single(board):
-    # if subtract_flags(board[y][x])# +  == #旗なしclosed - 畑ありclosed
-    # if cellの数字が周りのclosed-floaggedと同じだったら、全部旗つけ
-    # if 上のを終えて、cellの周りの旗がcellと同じ数になったら、全部開く
-    # listにattach
+    """Solve the board only by referring single cell.
+
+    If a number cell
+        1. has exactly same number of CLOSED cells around, then all of them can be FLAGGED
+        2. is zero, all cells around can be OPENED.
+    A "number" is actually (shown number - flags around)
+    """
     result_list = []
-    height = len(board)
-    width = len(board[0])
-    for y in range(height):
-        for x in range(width):
-            if __is_number(board[y][x]):
-                closed = __get_cell_around(board, y, x, Const.Cell.CLOSED)
-                real_number = board[y][x][0] - len(__get_cell_around(board, y, x, Const.Cell.FLAGGED))
-                if real_number == len(closed) != 0:
-                    for cell in closed:
-                        if {"type": Const.CellAction.FLAG, "coord": [cell[0], cell[1]]} not in result_list:
-                            result_list.append({"type": Const.CellAction.FLAG, "coord": [cell[0], cell[1]]})
-                elif real_number == 0:
-                    for cell in closed:
-                        if {"type": Const.CellAction.OPEN, "coord": [cell[0], cell[1]]} not in result_list:
-                            result_list.append({"type": Const.CellAction.OPEN, "coord": [cell[0], cell[1]]})
+    for y, x in ((y, x) for y in range(len(board)) for x in range(len(board[0]))):
+        if __is_number(board[y][x]):
+            closed = __get_cell_around(board, y, x, Const.Cell.CLOSED)
+            real_number = board[y][x][0] - len(__get_cell_around(board, y, x, Const.Cell.FLAGGED))
+            if real_number == len(closed) != 0:
+                for cell in closed:
+                    if {"type": Const.CellAction.FLAG, "coord": [cell[0], cell[1]]} not in result_list:
+                        result_list.append({"type": Const.CellAction.FLAG, "coord": [cell[0], cell[1]]})
+            elif real_number == 0:
+                for cell in closed:
+                    if {"type": Const.CellAction.OPEN, "coord": [cell[0], cell[1]]} not in result_list:
+                        result_list.append({"type": Const.CellAction.OPEN, "coord": [cell[0], cell[1]]})
     return result_list
 
 
 def __is_number(cell):
-    if (cell == Const.Cell.ONE or cell == Const.Cell.TWO
-        or cell == Const.Cell.THREE or cell == Const.Cell.FOUR or cell == Const.Cell.FIVE
-        or cell == Const.Cell.SIX or cell == Const.Cell.SEVEN or cell == Const.Cell.EIGHT):
+    if (cell == Const.Cell.ONE or cell == Const.Cell.TWO or cell == Const.Cell.THREE
+        or cell == Const.Cell.FOUR or cell == Const.Cell.FIVE or cell == Const.Cell.SIX
+        or cell == Const.Cell.SEVEN or cell == Const.Cell.EIGHT):
         return True
     else:
         return False
@@ -85,23 +84,26 @@ def __is_number(cell):
 
 def __get_cell_around(board, y, x, cell_type):
     cell_list = []
-    for yy in (-1, 0, 1):
-        for xx in (-1, 0, 1):
-            if (not (yy is 0 and xx is 0)
-                and 0 <= y + yy < len(board)
-                and 0 <= x + xx < len(board[0])
-                and board[y + yy][x + xx][0] is cell_type[0]):
-                cell_list.append([y + yy, x + xx])
+    for yyy, xxx in ((yy + y, xx + x) for yy in (-1, 0, 1) for xx in (-1, 0, 1)):
+        if (not (yyy == y and xxx == x)
+            and 0 <= yyy < len(board) and 0 <= xxx < len(board[0])
+            and board[yyy][xxx][0] is cell_type[0]):
+            cell_list.append((yyy, xxx))
     return cell_list
 
 
 def __solve_by_double(board):
-    # 隣り合う数字同士の差分と同じだけどちらかだけに隣接する未開放のセルがあれば、そこは確定する
+    """Solve the board by comparing with neighbor cell.
+
+    If abs(a number cell - its neighbor number cell) is same as either's non-shared cells count, those non-shared
+    cells will be determined.
+        1. If the subtracted value is positive, non-shared cells for the cell can be FLAGGED.
+        2. If the subtracted value is negative, non-shared cells for the cell can be OPENED.
+    """
     return []
 
 
 def __guess(board):
-    # 確率が一番少ないやつ適当にチョイス、あるいは0がなければ完全ランダムに。
+    """Choose one randomly from CLOSED cells."""
     closed = __get_cell_with(board, Const.Cell.CLOSED)
-    return [{"type": Const.CellAction.OPEN,
-             "coord": closed[random.randrange(len(closed))]}]
+    return [{"type": Const.CellAction.OPEN, "coord": closed[random.randrange(len(closed))]}]
